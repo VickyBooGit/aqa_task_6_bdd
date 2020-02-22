f5326edc11809058b5598f0c5849d0cd6d375d9f
Victoria Bukina <victoria.bukina@gmail.com>
2020-02-22 18:28:06 +0300

    Test added

---
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -0,0 +1,38 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+    }
+
+    public Integer getFirstCardBalance() {
+        String firstCard = $(By.cssSelector("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']")).getText();
+        String stringBalance = firstCard.substring(firstCard.indexOf(":") + 1, firstCard.indexOf("р.") - 1).trim();
+        return Integer.parseInt(stringBalance);
+    }
+
+    public Integer getSecondCardBalance() {
+        String secondCard = $(By.cssSelector("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']")).getText();
+        String stringBalance = secondCard.substring(secondCard.indexOf(":") + 1, secondCard.indexOf("р.") - 1).trim();
+        return Integer.parseInt(stringBalance);
+    }
+
+    public MoneyTransferPage pushMakeDepositFirstAccount(){
+        $(By.cssSelector("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']")).$(By.cssSelector("[data-test-id=action-deposit]")).click();
+        return new MoneyTransferPage();
+    }
+    public MoneyTransferPage pushMakeDepositSecondAccount(){
+        $(By.cssSelector("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']")).$(By.cssSelector("[data-test-id=action-deposit]"));
+        return new MoneyTransferPage();
+    }
+
+
+}
